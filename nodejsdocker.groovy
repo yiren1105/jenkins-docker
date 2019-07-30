cbac468244519eeb825c2ad83d5f6a69d205c870
yiren1105 <35779946+yiren1105@users.noreply.github.com>
2019-07-30 11:02:26 -0300

    Create nodejsdocker.groovy

---
+++ nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Yi Ren')
+            node / gitConfigEmail('yiren1105@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('yiren1105docker-jenkins')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
